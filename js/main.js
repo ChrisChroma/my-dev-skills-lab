84750c7ea1a186ef537975117cf1e0ec43d13ea1
ChrisChroma <chrischroma1030@gmail.com>
2020-05-10 08:01:13 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-$(document).ready(function() {
-    $('.startSkill').hide();
-});
+// $(document).ready(function() {
+//     $('.startSkill').hide();
+// });
 
 $('#addSkill').click(function() {
     const $skill = $('input').val()
-    $('#skills').append('<button>X</button> ' + $skill + '<br>')
+    $("tbody").append("<tr><td><button>X</button> " + $skill + "</td></tr>");
+    $('input').val('');
+    $('button').addClass('removeSkill');
+});
+
+$("#skills tbody").on("click", "button", function (e) {
+  // got the element we clicked on with e.target
+  // we used the closest method to find the able row we are inside of
+  // then we used .remove to remove it
+  //    $(e.target).closest('tr').remove();
+  console.log(this, "< outside the fadeOut method");
+
+  // find out the element then remove it
+  $(this)
+    .closest("tr")
+    .fadeOut(1500, function () {
+      console.log(this, " inside of our callback");
+      $(this).remove();
+    });
 });
